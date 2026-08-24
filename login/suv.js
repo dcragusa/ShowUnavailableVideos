55926b20f508f0ff157ccaee5bb066cd82771a5f
dcragusa <dcragusa@yahoo.com>
2026-08-23 19:53:11 -0700

    Split login into separate page because Google makes it a royal pain to get approval to allow logins to the API

--- login/suv.js
+++ login/suv.js
@@ -1,6 +1,47 @@
 const API_KEY = 'AIzaSyCPbFD7_-Dfb6694tKO4Q7nymekTRmLubQ';
 const API_ROOT = 'https://www.googleapis.com/youtube/v3/';
+const CLIENT_ID = '527549401887-jhfhpleebqkf17ubhqn7tvld896m10vc.apps.googleusercontent.com';
 const HEADERS = {'headers': {'Accept': 'application/json'}};
+let OWNED_PLAYLISTS = new Set();
+
+const client = google.accounts.oauth2.initTokenClient({
+  client_id: CLIENT_ID,
+  scope: 'https://www.googleapis.com/auth/youtube',
+  ux_mode: 'popup',
+  callback: oauthResponse,
+});
+
+async function oauthResponse(clientResponse) {
+  HEADERS['headers']['Authorization'] = `Bearer ${clientResponse.access_token}`;
+  await getOwnedPlaylists();
+  hideElement("login");
+  showElement("loggedIn");
+}
+
+async function getOwnedPlaylists() {
+  // Gets a list of playlists owned by the current user
+  const params = new URLSearchParams(
+    {'key': API_KEY, 'part': 'id', 'maxResults': '50', 'mine': 'true'}
+  );
+  let first = true;
+  let nextPageToken = null;
+  while (first || nextPageToken) {
+    if (nextPageToken) {
+      params.delete('pageToken');
+      params.append('pageToken', nextPageToken);
+    }
+    const response = await fetch(`${API_ROOT}playlists?${params.toString()}`, HEADERS)
+      .then(response => response.json());
+    if ('error' in response)
+      throw (response.error.code === 404 ? 'Could not find owned playlists.' : response.error.message);
+    console.log(response)
+    for (const item of response.items)
+      OWNED_PLAYLISTS.add(item['id']);
+    nextPageToken = response['nextPageToken'];
+    first = false;
+  }
+  console.log(OWNED_PLAYLISTS);
+}
 
 function hideElement(elementId) {
   // Adds the 'hidden' class to an element given its ID
@@ -79,7 +120,7 @@ async function getUnavailableVideos(videoIds, countryCode) {
   return [availableVideos, blockedVideos];
 }
 
-function outputRow(videoId, videoName) {
+function outputRow(videoId, videoName, isOwnedPlaylist, playlistItemId) {
   // Outputs a row of the results table
   const row = document.createElement('tr');
 
@@ -117,10 +158,24 @@ function outputRow(videoId, videoName) {
   cell4.setAttribute('class', 'align-middle');
   row.appendChild(cell4);
 
+  // If the playlist is owned by the user, provide removal buttons
+  if (isOwnedPlaylist) {
+    // Search for the name if present, otherwise search for the video ID
+    const cell5 = document.createElement('td');
+    cell5.setAttribute('id', `removal-${playlistItemId}`);
+    const removalElement = document.createElement('button');
+    removalElement.setAttribute('type', 'button');
+    removalElement.setAttribute('class', 'btn btn-link btn-sm align-middle');
+    removalElement.setAttribute('onclick', `removePlaylistItem("${playlistItemId}")`);
+    removalElement.innerHTML = 'Remove';
+    cell5.appendChild(removalElement);
+    row.appendChild(cell5);
+  }
+
   return row;
 }
 
-function outputToTable(videoIds, availableVideos, blockedVideos) {
+function outputToTable(videoIds, availableVideos, blockedVideos, isOwnedPlaylist) {
   // Outputs the results table
   const tableBody = document.getElementById('tablebody');
   // Calling this with no arguments removes all children
@@ -128,8 +183,24 @@ function outputToTable(videoIds, availableVideos, blockedVideos) {
   for (const videoId of videoIds.keys()) {
     if (availableVideos.has(videoId))
       continue;
-    const row = outputRow(videoId, blockedVideos[videoId]);
+    const row = outputRow(videoId, blockedVideos[videoId], isOwnedPlaylist, videoIds.get(videoId));
     tableBody.appendChild(row);
+  }
+}
+
+async function removePlaylistItem(playlistItemId) {
+  // Deletes a given playlist item (i.e., removes a video from the user's playlist)
+  const params = new URLSearchParams({'key': API_KEY, 'id': playlistItemId});
+  const request = new Request(
+    `${API_ROOT}playlistItems?${params.toString()}`,
+    {headers: HEADERS['headers'], method: 'DELETE'}
+  );
+  const response = await fetch(request);
+  if (response.status === 204)
+    document.getElementById(`removal-${playlistItemId}`).innerHTML = '✅ Removed';
+  else {
+    const json = await response.json();
+    document.getElementById(`removal-${playlistItemId}`).innerHTML = `❌ Failed (${json['error']['message']})`;
   }
 }
 
@@ -140,8 +211,13 @@ window.addEventListener('load', async function () {
     const countryCode = getFormValue('countryCode');
     if (!playlistId || !countryCode)
       return;
+    const ownedPlaylist = OWNED_PLAYLISTS.has(playlistId);
 
     hideElement('table');
+    if (ownedPlaylist)
+      showElement('removeFromPlaylist');
+    else
+      hideElement('removeFromPlaylist');
     hideElement('resultsText');
     hideElement('errorText');
     showElement('spinner');
@@ -151,7 +227,7 @@ window.addEventListener('load', async function () {
       const [availableVideos, blockedVideos] = await getUnavailableVideos(videoIds, countryCode);
       hideElement('spinner');
       if (videoIds.size !== availableVideos.size) {
-        outputToTable(videoIds, availableVideos, blockedVideos);
+        outputToTable(videoIds, availableVideos, blockedVideos, ownedPlaylist);
         showElement('table');
       } else
         showElement('resultsText');
